051326f29d7f4384e4d54c6442ac03419b1d97cd
Adam Allgood <allgoodadam@gmail.com>
2015-11-05 21:48:29 -0800

    done with what i can

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :like_instances, class_name: "Like"
+  has_many :like_instances, class_name: "Like", dependent: :destroy
   belongs_to :user
   has_many :liked_by, class_name: "User", through: :likes
 
