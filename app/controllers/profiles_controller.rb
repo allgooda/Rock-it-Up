efe10ea570d0bb7d726063234d225e3034e4295a
Adam Allgood <allgoodadam@gmail.com>
2015-11-05 18:23:46 -0800

    a monster change

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,14 +3,12 @@ class ProfilesController < ApplicationController
   def index
     @posts = Post.all
 
-    @likes = Like.where(user_id:current_user.id)
+    @likes = Like.where(user_id: current_user.id)
     @post_ids = []
 
     @likes.each do |like|
       @post_ids << like.post_id
     end
-
   end
-
 end
 
