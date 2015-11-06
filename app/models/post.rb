104c2be85782b472a2b647cea653cc58f464a83f
Adam Allgood <allgoodadam@gmail.com>
2015-11-05 18:28:47 -0800

    fixed delete error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :like_instances, class_name: "Like"
   belongs_to :user
-  has_and_belongs_to_many :liked_by, class_name: "User"
+  has_many :liked_by, class_name: "User", through: :likes
 
   def likes
     like_instances.length
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts, class_name: "Post"
-  has_and_belongs_to_many :liked, class_name: "Post"
+  has_many :liked, class_name: "Post", through: :likes
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
